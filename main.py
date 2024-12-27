d0dc84acf74a608a500c18788056171fd177ba5d
Randy La <randyla78@gmail.com>
2024-12-27 00:20:03 -0500

    Added visual graphs

--- main.py
+++ main.py
@@ -1,66 +1,85 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from tensorflow.keras import datasets, layers, models
 
-#get datasets from keras (in the format of two tuples)
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
-#scale data down
+
 training_images = training_images / 255
 testing_images = testing_images / 255
 
 class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
 
-#display 16 images
+plt.figure(figsize=(8, 8))
 for i in range(16):
-    plt.subplot(4,4,i+1)
+    plt.subplot(4, 4, i + 1)
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.imshow(training_images[i], cmap=plt.cm.binary, interpolation='bicubic')
     plt.xlabel(class_names[training_labels[i][0]])
-
+plt.tight_layout()
 plt.show()
 
-#take first 20k to train neural network
-training_images = training_images[:20000]
-training_labels = training_labels[:20000]
-testing_images = testing_images[:4000]
-testing_labels = testing_labels[:4000]
+training_images = training_images[:50000]
+training_labels = training_labels[:50000]
+testing_images = testing_images[:10000]
+testing_labels = testing_labels[:10000]
 
-
-#-=(+)=-
-#building neural network
-model = models.Sequential() #define neural network
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3))) #define input layer
-#^to know more abt activation and neural network, waych intro to neural network vid by nerualnine
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu')) #filters for features in an image
-model.add(layers.MaxPooling2D((2,2))) #MaxPooling reduces image to the essential information
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax')) #scales results so that probabilities add up to 1
+model.add(layers.Dense(10, activation='softmax'))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels)) #10 epochs mean model will see the same data 10 times
+history = model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
 
 loss, accuracy = model.evaluate(testing_images, testing_labels)
 print(f"Loss: {loss}")
 print(f"Accuracy: {accuracy}")
 
-#run with this line to save model (so that we dont have to train model from scratch every time we run the program
-#model.save('image_classifier.model')
+plt.figure(figsize=(12, 5))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
 
-#-=(+)=-
-#run with this line after saving to load the previously saved model
-#model = models.load_model('image_classifier.model') #Make sure to comment everything between the two -=(+)=- before loading
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.tight_layout()
+plt.show()
 
+#Predictions and Confusion Matrix
+predictions = model.predict(testing_images)
+predicted_labels = np.argmax(predictions, axis=1).flatten()
+true_labels = testing_labels.flatten()
 
-#run code below after loading in order to test any image from google (make sure to convert image to 32x32)
-img = cv.imread('horse.jpg')
-img = cv.cvtcolor(img, cv.COLOR_BGR2GRB)
-plt.imshow(img, cmap=plt.cm.binary)
-prediction = model.predict(np.array([img]) / 255)
-index = np.argmax(prediction)
-print(f'Prediction is {class_names[index]}')
+cm = confusion_matrix(true_labels, predicted_labels)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+disp.plot(cmap=plt.cm.Blues, xticks_rotation=45)
+plt.title('Confusion Matrix')
+plt.show()
+
+#Accuracy
+class_accuracy = cm.diagonal() / cm.sum(axis=1)
+plt.figure(figsize=(10, 5))
+plt.bar(class_names, class_accuracy)
+plt.xlabel('Class')
+plt.ylabel('Accuracy')
+plt.title('Class-wise Accuracy')
+plt.ylim(0, 1)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
